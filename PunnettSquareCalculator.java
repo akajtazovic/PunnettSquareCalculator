a2d917ddb150670e53da5899efd9b640e8fe3d0f
akajtazovic <60759369+akajtazovic@users.noreply.github.com>
2022-04-25 17:43:36 -0500

    Add files via upload

--- PunnettSquareCalculator.java
+++ PunnettSquareCalculator.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.awt.Scanner;
+import java.util.Scanner;
 
 //OvalDrawPlus inheriting from JFrame
 public class PunnettSquareCalculator extends JFrame {
@@ -16,12 +16,12 @@ static final int GREEN = 20;
 static final int BLUE = 60;
 
 //OvalDrawPlus Method
-	public OvalDrawPlus() {
-		super("OvalDrawPlus");
+	public PunnettSquareCalculator() {
+		super("Congratulations!");
 		//setting close option.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//setting size of JPanel
-		setSize(500,400);
+		setSize(500,500);
 		//setting location of JPanel
 		setLocation(250,0);
 		//making JPanel visible
@@ -35,7 +35,7 @@ static final int BLUE = 60;
 		addWindowListener (new java.awt.event.WindowAdapter() {
 			@Override
 			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-				System.out.println("Closing OvalDrawPlus... ");
+				System.out.println("We hope you enjoyed (:) ");
 			}
 		});
 
@@ -45,23 +45,17 @@ static final int BLUE = 60;
 		super.paint(g);
 		int width = getWidth();
 		int height = getHeight();
-
-		//drawing dark blue rectangle, then purple oval
-		g.drawRect(0,3,width,height);
-		//.darker() makes the blue color darker.
-		g.setColor(Color.BLUE.darker());
-		//filling dark blue color in rectangle
-		g.fillRect(0,3,width,height);
-		//drawing oval
-		g.drawOval(0,3,width,height);
-
-
-		//creating purple color with RGB
-		Color NewColor = new Color(RED,GREEN,BLUE);
-		g.setColor(NewColor);
-
-		//filling oval
-		g.fillOval(0,3,width,height);
+		// Oval for face outline
+        g.drawOval(80, 70, 150, 150);
+ 
+        // Ovals for eyes
+        // with black color filled
+        g.setColor(Color.BLACK);
+        g.fillOval(120, 120, 15, 15);
+        g.fillOval(170, 120, 15, 15);
+ 
+        // Arc for the smile
+        g.drawArc(130, 180, 50, 20, 180, 180);
 	}
 
 	//main class
@@ -70,6 +64,8 @@ static final int BLUE = 60;
 		System.out.println("What is the percentage of your child having brown hair?");
 		System.out.println("Examples: BB = Both Brown hair genes are dominant. Bb = one gene is dominant, one is negative. bb = both are recessive (meaning blonde hair). ");
 		System.out.println("*****************************************************************");
+		System.out.println("Capital B first please");
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter your first hair genotype(B or b): ");
 		String yourgeneone = sc.nextLine();
@@ -79,8 +75,37 @@ static final int BLUE = 60;
 		String partnergeneone = sc.nextLine();
 		System.out.println("Please enter your patner's second hair gene(B or b): ");
 		String partnergenetwo = sc.nextLine();
-		if (yourgeneone = "B" || yourgenetwo = "b" && partnergeneone = "B"
 
-		OvalDrawPlus draw = new OvalDrawPlus();
+		if (yourgeneone == "B" & yourgenetwo == "b" && partnergeneone == "B" & partnergenetwo == "b") {
+			System.out.println("Your child has a 75% chance of having brown hair.");
+			PunnettSquareCalculator dr = new PunnettSquareCalculator();
+		} 
+		else if (yourgeneone == "B" && yourgenetwo == "B" && partnergeneone == "B" && partnergenetwo == "b") {
+			System.out.println("Your child has a 100% chance of developing brown hair.");
+			PunnettSquareCalculator dr = new PunnettSquareCalculator();
+		}
+		else if (yourgeneone == "B" && yourgenetwo == "B" && partnergeneone == "B" && partnergenetwo == "B") {
+			System.out.println("Your child has a 100% chance of developing brown hair");
+			PunnettSquareCalculator dr = new PunnettSquareCalculator();
+		}
+		else if (yourgeneone == "b" && yourgenetwo == "b" && partnergeneone == "b" && partnergenetwo == "b") {
+			System.out.println("Your child has no chance of developing brown hair");
+		}
+		else if (yourgeneone == "B" && yourgenetwo == "b" && partnergeneone == "B" && partnergenetwo == "B") {
+			System.out.println("Your child has a 100% chance of developing brown hair.");
+			PunnettSquareCalculator dr = new PunnettSquareCalculator();
+		}
+		else if (yourgeneone == "b" && yourgenetwo == "b" && partnergeneone == "B" && partnergenetwo == "b") {
+			System.out.println("Your child has a 50% chance of developing brown hair.");
+			OvalDrawPlus drawin = new OvalDrawPlus();
+		}
+		else if (yourgeneone == "B" && yourgenetwo == "b" && partnergeneone == "b" && partnergenetwo == "b") {
+			System.out.println("This child has a 50% chance of developing brown hair.");
+			PunnettSquareCalculator dr = new PunnettSquareCalculator();
+		} else {
+			System.out.println("Error. Please try again.");
+		}
+
+		//OvalDrawPlus draw = new OvalDrawPlus();
 	}
-}
+}	
